a8249cbce7d340b21968bc1055deea8c69d9b3fa
Alistair Ponce <alistair.ponce@gmail.com>
2024-10-09 13:44:19 +0900

    update

--- src/admin/settings/tabs/general.php
+++ src/admin/settings/tabs/general.php
@@ -1,1 +1,30 @@
-General settings here
+<?php
+ff_modules_load_settings_page();
+$settings = new FF_Modules_Settings_Page();
+?>
+
+<form action="" method="POST">
+
+    <?php
+    // $settings->input_text([
+    //     'key' => 'sample_input',
+    //     'label' => 'Sample Input',
+    // ]);
+
+    // $settings->textarea([
+    //     'key' => 'sample_textarea',
+    //     'label' => 'Sample textarea',
+    // ]);
+
+    // $settings->select([
+    //     'key' => 'sample_select',
+    //     'label' => 'Sample Select',
+    //     'options' => [
+    //         'option_value' => 'Option Label',
+    //     ]
+    // ]);
+
+    $settings->save_button();
+    ?>
+
+</form>
